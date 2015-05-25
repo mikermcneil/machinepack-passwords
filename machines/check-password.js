eb32b1e623b6da78a5c6bb7467d6e30bc124af1d
Mike McNeil <mike@balderdash.co>
2015-05-24 23:11:35 -0500

    Remove extraneous stuff.

--- machines/check-password.js
+++ machines/check-password.js
@@ -1,16 +1,24 @@
 module.exports = {
+
+
   friendlyName: 'Check password',
+
+
   description: 'Compare a plaintext password attempt against an already-encrypted version.',
+
+
   extendedDescription: 'Useful for checking a password attempt against the stored, already-encrypted BCrypt hash.',
+
+
   inputs: {
-    'passwordAttempt': {
+    passwordAttempt: {
       example: 'l0lcatzz',
       friendlyName: 'Password attempt',
       description: 'The password attempt (unencrypted).',
       required: true,
       protect: true
     },
-    'encryptedPassword': {
+    encryptedPassword: {
       example: 'as34hafsu#w34ndcarok',
       friendlyName: 'Encrypted password',
       description: 'The existing (already-encrypted) password hash to compare against.',
@@ -18,20 +26,21 @@ module.exports = {
       protect: true
     }
   },
-  defaultExit: 'success',
+
+
   exits: {
-    'success': {
-      void: true,
+    success: {
       description: 'Password attempt matches already-encrypted version'
     },
-    'incorrect': {
-      void: true,
+    incorrect: {
       description: 'Password attempt does not match already-encrypted version'
     },
-    'error': {
+    error: {
       description: 'An unexpected error occurred'
     }
   },
+
+
   fn: function(inputs, exits) {
     require('bcrypt-nodejs').compare(inputs.passwordAttempt, inputs.encryptedPassword, function(err, ok) {
       if (err) {
--- machines/encrypt-password.js
+++ machines/encrypt-password.js
@@ -1,8 +1,17 @@
 module.exports = {
+
+
   friendlyName: 'Encrypt password',
+
+
   description: 'Encrypt a string using the BCrypt algorithm.',
+
+
   extendedDescription: 'This is particularly useful for encrypting user passwords before storing them in the database.',
+
+
   inputs: {
+
     password: {
       example: 'l0lcatzz',
       friendlyName: 'Password',
@@ -10,26 +19,31 @@ module.exports = {
       required: true,
       protect: true
     }
+
   },
-  defaultExit: 'success',
+
+
   exits: {
+
     success: {
       example: '2$a492.abc3fadifhoi3hesdqd',
       description: 'Password was successfully encrypted.'
     },
+
     error: {
       description: 'An unexpected error occured.'
     }
-  },
-  fn: function(inputs, exits) {
 
+  },
+
+
+  fn: function(inputs, exits) {
     require('bcrypt-nodejs').hash(inputs.password, null , null, function(err, hash) {
       if (err) {
         return exits.error(err);
       }
       return exits.success(hash);
     });
-
   }
 
 };
